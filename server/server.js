cd2e4a51804e1c5034da19f0cac2cf717b3a9527
Prashanth Kumar hirematam <prashanthkumarhirematam@gmail.com>
2017-12-19 18:13:14 +0530

    Add DET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,6 +31,23 @@ app.get('/todos',(req,res) => {
     });
 });
 
+//Dynamic routing
+app.get('/todos/:id',(req,res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    Todo.findById({_id:id}).then((todo)=>{
+        if(!todo){
+           return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
