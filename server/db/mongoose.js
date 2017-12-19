5b74847d7661a8ff6cd4638f480453d22c3a4b36
Prashanth Kumar hirematam <prashanthkumarhirematam@gmail.com>
2017-12-19 23:21:38 +0530

    changing to moongose.js 3rd time

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://mepraash:amma@143@ds161316.mlab.com:61316/todosapp'
+    mlab: 'mongodb://mepraash:12345678@ds161316.mlab.com:61316/todosapp'
   };
 
   mongoose.connect( process.env.PORT ? db.mlab : db.localhost,{
