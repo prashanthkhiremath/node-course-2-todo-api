207596361323284b0f8b8f7996f93a51106c74e0
Prashanth Kumar hirematam <prashanthkumarhirematam@gmail.com>
2017-12-21 13:47:05 +0530

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -32,23 +32,23 @@ app.get('/todos',(req,res) => {
     });
 });
 
-// //Dynamic routing
-// app.get('/todos/:id',(req,res) => {
-//     var id = req.params.id;
+//Dynamic routing
+app.get('/todos/:id',(req,res) => {
+    var id = req.params.id;
 
-//     if(!ObjectID.isValid(id)){
-//         return res.status(404).send()
-//     }
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
 
-//     Todo.findById({_id:id}).then((todo)=>{
-//         if(!todo){
-//            return res.status(404).send();
-//         }
-//         res.send({todo});
-//     }).catch((e) => {
-//         res.status(400).send();
-//     })
-// })
+    Todo.findById({_id:id}).then((todo)=>{
+        if(!todo){
+           return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.delete('/todos/:id',(req,res) => {
     var id = req.params.id;
@@ -57,11 +57,11 @@ app.delete('/todos/:id',(req,res) => {
         return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((result) => {
-        if(!result) {
-            return res.status(404);
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
         }
-        res.status(200).send(result);
+        res.status(200).send({todo});
     }).catch((e) => {
         res.status(400).send();
     })    
