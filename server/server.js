9e7ee0959826df8baac0eddb9c2d9cb3abae252f
Prashanth Kumar hirematam <prashanthkumarhirematam@gmail.com>
2017-12-21 21:31:00 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 
 var express = require('express');
@@ -65,6 +66,32 @@ app.delete('/todos/:id',(req,res) => {
     }).catch((e) => {
         res.status(400).send();
     })    
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+       body.completedat = new Date().getTime(); 
+    } else {
+       body.completed = false;
+       body.completedat = null; 
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
 });
 
 
