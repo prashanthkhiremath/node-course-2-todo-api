ad75053ec48b45a96620d9fceb195ab2175a1c62
Prashanth Kumar hirematam <prashanthkumarhirematam@gmail.com>
2017-12-25 19:51:06 +0530

    Add GET /users/me and authentication middleware

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -33,6 +34,7 @@ var UserSchema = new mongoose.Schema({
 });
 
 UserSchema.methods.generateAuthToken = function () {
+    //this represnts individual document
     var user = this;
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
@@ -43,6 +45,32 @@ UserSchema.methods.generateAuthToken = function () {
         return token;
     })
 };
+
+//Model method
+
+UserSchema.statics.findByToken = function(token) {
+//model method will be called with model as this
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token, 'abc123')
+    } catch (e) {
+        // return new Promise((resolve,reject) => {
+        //     reject();
+        // });
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+   
+};
+
+
 
 
 var Users = mongoose.model('Users', UserSchema);
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
 var {Users} = require('./models/users');
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express();
 const port = process.env.PORT;
@@ -108,6 +109,10 @@ app.post('/users',(req,res) => {
         res.status(400).send(e);
     });
 })
+
+app.get('/users/me', authenticate, (req,res) => {
+    res.send(req.user);
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
